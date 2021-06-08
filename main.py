ba38553668705bc64d4dc8ba09d064d2bfc020fa
Sefa <mail@sefakalkan.com>
2021-06-08 11:28:43 +0300

    0.2

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import schedule
 
 
 x = random.poisson(lam=4, size=3)
-y = random.poisson(lam=20, size=3)
+y = random.poisson(lam=10, size=3)
+
+
 
 schedule.MLFQ(x,y) #Multilevel feedback Queue scheduling
 
--- schedule.py
+++ schedule.py
@@ -1,13 +1,29 @@
 import req
 
 
+
+
+
+
+
+#Turnaround time = Exit time - Arrival time
+#Turnaround time = Burst time + Waiting time
+#Waiting time = Turnaround time - Burst time
+#Througput = waiting time /3
+
+
+class process:
+    def __init__(self,ID, start, end,):
+        self.ID = ID
+        self.start = start
+        self.end = end
+
+
 def MLFQ(x,y):
     print(x)
     print(y)
     temp = req.reqTime(y)
-
-
-    highPriority(x,y,(temp*0.5))#SJF
+    highPriority(x,y,(temp*0.5),100)#SJF
 
 
 
@@ -16,7 +32,9 @@ def MLFQ(x,y):
 
 
 
-def highPriority(x,y,ratio,averageTime):#SJF
+def highPriority(x, y, ratio, averageTime):#SJF
+    global idleTime #May i put waiting time to other scheduling algo's
+    idleTime=0
     tempX = x
     tempY = y
     temp = 0
@@ -26,7 +44,9 @@ def highPriority(x,y,ratio,averageTime):#SJF
             print(tempY[temp])
             tempY[temp] -=1
         else:
-            print("GAYYYYY")
-
-
+            idleTime += 1
+    print(":", idleTime)
     print(tempX,tempY)
+    return tempY
+
+
