d4d9bf36352d30a119114832a03cf1852cf471c8
Sefa <mail@sefakalkan.com>
2021-06-08 15:31:47 +0300

    0.4

--- schedule.py
+++ schedule.py
@@ -1,6 +1,15 @@
 import req
 
+"""
+Arrival Time: Time at which the process arrives in the ready queue.
+Completion Time: Time at which process completes its execution.
+Burst Time: Time required by a process for CPU execution.
+Turn Around Time: Time Difference between completion time and arrival time.
+Turn Around Time = Completion Time – Arrival Time
 
+Waiting Time(W.T): Time Difference between turn around time and burst time.
+Waiting Time = Turn Around Time – Burst Time
+"""
 
 
 
@@ -26,19 +35,15 @@ def MLFQ(x,y):
     global pList
     pList = []
     pList.append(process(0, -1, -1))
-    pList.append(process(0, -1, -1))
-    pList.append(process(0, -1, -2))
+    pList.append(process(1, -1, -1))
+    pList.append(process(2, -1, -1))
 
-    highPriority(x,y,(temp*0.5))#SJF
-    checkStartEnd(1,1)
+    tempY = highPriority(x,y,(temp*0.5))#SJF  hoca hepsine ayrı time derse buraya tempx
+    medPriority(x,tempY,(temp*0.3),5) # buraya da x yerine tempX yaz
 
-
-
-
-
-
-
-
+    print("vololo2")
+    printList()
+    print("vololo2")
 
 
 def highPriority(x, y, ratio):#SJF
@@ -50,20 +55,27 @@ def highPriority(x, y, ratio):#SJF
     for i in range(int(ratio)):
         temp = req.findShortest(tempX,tempY)
         if (temp != -1):
-            print(i," : ",tempY[temp])
-            print(temp)
             tempY[temp] -=1
+            checkStartEnd(i,temp,tempY[temp])
+
         else:
             idleTime += 1
-    print(":", idleTime)
-    print(tempX,tempY)
-    return tempY
 
-def checkStartEnd(time,ID):
-    for x in pList:
-        print(x.end)
-        x.end=1
+    return tempX,tempY
+
+def medPriority(x,tempY, ratio,q):
+    for x in range(int(ratio)):
+        if(x%q == 0)
+
+def checkStartEnd(time,ID,var):
+    for p in pList:
+        if(p.ID == ID):
+            if(p.start == -1):
+                p.start = time
+            elif(p.end == -1 and var == 0):
+                p.end = (time+1)
 
 
-
-
+def printList():
+    for obj in pList:
+        print(obj.ID, obj.start,obj.end, sep=' ')
