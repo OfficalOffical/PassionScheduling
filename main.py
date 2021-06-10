d7547a3f8d24dad176aefd37d0dd2b9a8346948a
Sefa <mail@sefakalkan.com>
2021-06-10 11:43:55 +0300

    0.33 (Before Disaster)

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from numpy import random
 import schedule
 
 
-x = random.poisson(lam=4, size=5)
+x = random.poisson(lam=2, size=5)
 y = random.poisson(lam=10, size=5)
 
 
 
 schedule.MLFQ(x,y) #Multilevel feedback Queue scheduling
+
 
 
