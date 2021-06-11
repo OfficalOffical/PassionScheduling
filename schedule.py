43d6218117620b5cf0efab9b09270a8d60b80f05
Sefa <mail@sefakalkan.com>
2021-06-11 14:08:16 +0300

    0.55

--- schedule.py
+++ schedule.py
@@ -8,7 +8,7 @@ from numpy import random
 
 
 class process:
-    def __init__(self, ID, start, end, ):
+    def __init__(self, ID, start, end):
         self.ID = ID
         self.start = start
         self.end = end
@@ -16,36 +16,13 @@ class process:
 
 def MLFQ():
 
-    x1 = random.poisson(lam=2, size=15)
-    y1 = random.poisson(lam=10, size=15)
-    x2 = random.poisson(lam=2, size=10)
-    y2 = random.poisson(lam=10, size=10)
-    x3 = random.poisson(lam=2, size=10)
-    y3 = random.poisson(lam=10, size=10)
+    x1,y1,x2,y2,x3,y3,timeSum = init(lamX=4,lamY=4,lamZ=4)
 
-    timeSum = req.reqTime(y1)
-    timeSum += req.reqTime(y2)
-    timeSum += req.reqTime(y3)
-    print("MasterClock : ",timeSum)
+    t1 = highPriority(x1, y1, (timeSum * 0.5))  # SJF  hoca hepsine ayrı time derse buraya tempx
+    t2 = medPriority(y2, (timeSum * 0.3), (timeSum * 0.5))  # buraya da x yerine tempX yaz
+    t3 = lowPriority(y3, (timeSum * 0.8))
 
-    global idleTime
-    idleTime = 0
-
-    init(x1,x2,x3)
-
-
-    t1,tempY1  = highPriority(x1, y1, (timeSum * 0.5))  # SJF  hoca hepsine ayrı time derse buraya tempx
-    printLast(timeSum, t1, 1, 1)
-    print("Origin X1:", x1, "\nY1:", y1)
-    print("temp X1:",x1,"\nY1:",tempY1)
-    print("----------------------------------")
-    t2,tempY2 = medPriority(y2, (timeSum * 0.3), (timeSum * 0.5))  # buraya da x yerine tempX yaz
-    print("s2")
-    t3,tempY3 = lowPriority(y3, (timeSum * 0.8))
-
-
-
-    #printLast(timeSum,t1,t2,t3)
+    printLast(timeSum,t1,t2,t3)
 
 def highPriority(x, y, ratio):  # SJF
     # May i put waiting time to other scheduling algo's
@@ -59,11 +36,11 @@ def highPriority(x, y, ratio):  # SJF
         temp = req.findShortest(tempX, tempY)
         if (temp != -1):
             tempY[temp] -= 1
-            checkStartEnd(i, temp, tempY[temp])
+            checkStartEndHigh(i, temp, tempY[temp])
         else:
             idleTime += 1
 
-    return range(int(ratio)),tempY  # Buraya temp X ekleyebilirsin
+    return range(int(ratio))  # Buraya temp X ekleyebilirsin
 
 
 def medPriority(tempY, ratio, realTime):
@@ -78,8 +55,8 @@ def medPriority(tempY, ratio, realTime):
                     break;
                 else:
                     if ((tempY[temp] - 1) == 0):
-                        checkStartEnd((realTime + time), temp, 0)
-                    checkStartEnd((realTime + time), temp, -1)
+                        checkStartEndMed((realTime + time), temp, 0)
+                    checkStartEndMed((realTime + time), temp, -1)
                     tempY[temp] -= 1
                     time += 1
 
@@ -89,8 +66,8 @@ def medPriority(tempY, ratio, realTime):
                     break
                 else:
                     if ((tempY[temp] - 1) == 0):
-                        checkStartEnd((realTime + time), temp, 0)
-                    checkStartEnd((realTime + time), temp, -1)
+                        checkStartEndMed((realTime + time), temp, 0)
+                    checkStartEndMed((realTime + time), temp, -1)
                     tempY[temp] -= 1
                     time += 1
 
@@ -98,31 +75,30 @@ def medPriority(tempY, ratio, realTime):
         if (time == ratio):
             break
 
-    return time,tempY
+    return time
 
 
 def lowPriority(y, realTime):
     realTime = int(realTime)
     time = 0
     temp = 0
-    p = pList
     while True:
         while True:
             if (y[temp] == 0):
                 break
             if ((y[temp] - 1) == 0):
-                checkStartEnd((realTime + time), temp, 0)
+                checkStartEndLow((realTime + time), temp, 0)
                 y[temp] -= 1
                 time += 1
                 break
             else:
-                checkStartEnd((realTime + time), temp, -1)
+                checkStartEndLow((realTime + time), temp, -1)
                 y[temp] -= 1
                 time += 1
-        temp = ((temp + 1) % len(pList))
+        temp = ((temp + 1) % len(pList3))
         if (endCheck()):
             break
-    return time,y
+    return time
 
 
 def checkStartEndHigh(time, ID, var):
@@ -133,16 +109,24 @@ def checkStartEndHigh(time, ID, var):
             elif (p.end == -1 and var == 0):
                 p.end = (time + 1)
 
+
+
 def checkStartEndMed(time, ID, var):
-    for p in pList:
+    for p in pList2:
         if (p.ID == ID):
+            if(var == 0):
+                print("-> P : ", p.ID, ":", p.start, "Time : ", p.end, "Var :", var)
             if (p.start == -1):
                 p.start = time
+                print("Start : P : ", p.ID, ":", p.start, "Time : ", p.end)
             elif (p.end == -1 and var == 0):
                 p.end = (time + 1)
 
+
+
+
 def checkStartEndLow(time, ID, var):
-    for p in pList:
+    for p in pList3:
         if (p.ID == ID):
             if (p.start == -1):
                 p.start = time
@@ -151,30 +135,56 @@ def checkStartEndLow(time, ID, var):
 
 
 def endCheck():
-    for obj in pList:
+    for obj in pList3:
         if(obj.end == -1):
             return False
     return True
 
 def printLast(globalTime,t1,t2,t3):
     for obj in pList:
-        print("P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+        print("High P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+    print("/////////////////////////////////////////////////")
+    for obj in pList2:
+        print("Med P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+    print("/////////////////////////////////////////////////")
+    for obj in pList3:
+        print("Low P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
     #for obj in pList:
         #print(obj.ID, obj.start, obj.end, sep=' ')
 
-def init(x1,x2,x3):
+def init(lamX,lamY,lamZ):
+    x1 = random.poisson(lam=lamX, size=10)
+    y1 = random.poisson(lam=10, size=10)
+    x2 = random.poisson(lam=lamY, size=10)
+    y2 = random.poisson(lam=10, size=10)
+    x3 = random.poisson(lam=lamZ, size=10)
+    y3 = random.poisson(lam=10, size=10)
+
+
+
+    timeSum = req.reqTime(y1)
+    timeSum += req.reqTime(y2)
+    timeSum += req.reqTime(y3)
+    print("MasterClock : ", timeSum)
+
     global pList
     global pList2
     global pList3
+    global idleTime
+
+    idleTime = 0
     pList = []
     pList2 = []
     pList3 = []
+
     for i in range(len(x1)):
         pList.append(process(i,-1,-1))
     for i in range(len(x2)):
         pList2.append(process(i,-1,-1))
     for i in range(len(x3)):
         pList3.append(process(i,-1,-1))
+
+    return x1,y1,x2,y2,x3,y3,timeSum
 
 
 """
