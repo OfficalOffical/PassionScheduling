137866bc1dfd7a5a696d44b2d97820ab546a80cf
Sefa KALKAN <mail@sefakalkan.com>
2021-06-13 23:18:32 +0300

    1.0

--- schedule.py
+++ schedule.py
@@ -16,22 +16,16 @@ class process:
 
 def MLFQ():
 
-    x1,y1,x2,y2,x3,y3,timeSum = init(lamX=4,lamY=4,lamZ=4)
+    runAndOptimize(10)
 
-    t1 = highPriority(x1, y1, (timeSum * 0.5))  # SJF  hoca hepsine ayrı time derse buraya tempx
-    t2 = medPriority(y2, (timeSum * 0.3), (timeSum * 0.5))  # buraya da x yerine tempX yaz
-    t3 = lowPriority(y3, (timeSum * 0.8))
 
-    printLast(timeSum,t1,t2,t3)
 
 def highPriority(x, y, ratio):  # SJF
-    # May i put waiting time to other scheduling algo's
-    global idleTime
     ratio = int(ratio)
-    print(ratio)
+    global idleTime
+    idleTime = 0
     tempX = x
     tempY = y
-    print(tempX,tempY)
     for i in range(ratio):
         temp = req.findShortest(tempX, tempY)
         if (temp != -1):
@@ -40,46 +34,53 @@ def highPriority(x, y, ratio):  # SJF
         else:
             idleTime += 1
 
-    return range(int(ratio))  # Buraya temp X ekleyebilirsin
+    if (idleTime <= 3):
+        idleTime = 0
+    return idleTime  # Buraya temp X ekleyebilirsin
 
 
-def medPriority(tempY, ratio, realTime):
-    realTime = int(realTime)
+def medPriority(tempY, ratio):
     ratio = int(ratio)
+    global idleMed
+    idleMed = 0
     time = 0
     temp = 0
     while True:
-        if ((tempY[temp] / 4) > 1.0):  # Az beynini zorlasan olcak sanki
+        if ((tempY[temp] / 4) >= 1.0):  # Az beynini zorlasan olcak sanki
             for i in range(4):
-                if (time == ratio):
+                if (time >= ratio):
                     break;
                 else:
                     if ((tempY[temp] - 1) == 0):
-                        checkStartEndMed((realTime + time), temp, 0)
-                    checkStartEndMed((realTime + time), temp, -1)
+                        checkStartEndMed((time), temp, 0)
+                    checkStartEndMed((time), temp, -1)
                     tempY[temp] -= 1
                     time += 1
 
+    #inf verirsen düzelebilir
         else:
             for i in range(tempY[temp]):
-                if (time == ratio):
+                if (time >= ratio):
                     break
                 else:
                     if ((tempY[temp] - 1) == 0):
-                        checkStartEndMed((realTime + time), temp, 0)
-                    checkStartEndMed((realTime + time), temp, -1)
+                        checkStartEndMed((time), temp, 0)
+                    checkStartEndMed((time), temp, -1)
                     tempY[temp] -= 1
                     time += 1
-
-        temp = ((temp + 1) % len(pList))
-        if (time == ratio):
+        temp = ((temp + 1) % len(pList2))
+        if (time >= ratio):
             break
+        if(endCheckMed()):
+            idleMed += 1
+            time += 1
+    return idleMed
 
-    return time
 
-
-def lowPriority(y, realTime):
-    realTime = int(realTime)
+def lowPriority(tempY,ratio):
+    y = tempY
+    global  idleLow
+    idleLow = 0
     time = 0
     temp = 0
     while True:
@@ -87,18 +88,21 @@ def lowPriority(y, realTime):
             if (y[temp] == 0):
                 break
             if ((y[temp] - 1) == 0):
-                checkStartEndLow((realTime + time), temp, 0)
+                checkStartEndLow((time), temp, 0)
                 y[temp] -= 1
                 time += 1
                 break
             else:
-                checkStartEndLow((realTime + time), temp, -1)
-                y[temp] -= 1
+                checkStartEndLow((time), temp, -1)
+                tempY[temp] -= 1
                 time += 1
         temp = ((temp + 1) % len(pList3))
-        if (endCheck()):
+        if ((time >= ratio)):
             break
-    return time
+        if(endCheck()):
+            time += 1
+            idleLow += 1
+    return idleLow
 
 
 def checkStartEndHigh(time, ID, var):
@@ -114,11 +118,8 @@ def checkStartEndHigh(time, ID, var):
 def checkStartEndMed(time, ID, var):
     for p in pList2:
         if (p.ID == ID):
-            if(var == 0):
-                print("-> P : ", p.ID, ":", p.start, "Time : ", p.end, "Var :", var)
             if (p.start == -1):
                 p.start = time
-                print("Start : P : ", p.ID, ":", p.start, "Time : ", p.end)
             elif (p.end == -1 and var == 0):
                 p.end = (time + 1)
 
@@ -141,31 +142,100 @@ def endCheck():
     return True
 
 def printLast(globalTime,t1,t2,t3):
+    waitHigh = 0
+    avTurnHigh = 0
+    waitMed = 0
+    avTurnMed = 0
+    waitLow = 0
+    avTurnLow = 0
     for obj in pList:
-        print("High P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+        if (obj.end == -1):
+            print( "High P", obj.ID, " doesnt finished" )
+        else:
+            waitHigh += obj.start
+            avTurnHigh += (obj.end - obj.start)
+            print( "High P", obj.ID, "-> Waiting Time :",obj.start,"Turn Around Time :", (obj.end - obj.start), )
+
     print("/////////////////////////////////////////////////")
     for obj in pList2:
-        print("Med P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+        if (obj.end == -1):
+            print( "Med P", obj.ID, " doesnt finished" )
+        else:
+            waitMed += obj.start
+            avTurnMed += (obj.end - obj.start)
+            print( "Med P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+
     print("/////////////////////////////////////////////////")
     for obj in pList3:
-        print("Low P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
-    #for obj in pList:
-        #print(obj.ID, obj.start, obj.end, sep=' ')
+        if (obj.end == -1):
+            print( "Low P", obj.ID, " doesnt finished" )
+        else:
+            waitLow += obj.start
+            avTurnLow += (obj.end - obj.start)
+            print( "Low P", obj.ID, "-> Waiting Time :", obj.start, "Turn Around Time :", (obj.end - obj.start), )
+
+    print( "P1-> Average waiting time : ", (waitHigh / len( pList )), "Utilization Time : ",
+           int(((tempTimeX - idleTime) / tempTimeX) * 100), " Throughput", tpAl( 1 ), "Average Turnaround time :",
+           avTurnHigh, "Idle time : ", t1, "Average Lenght : ", tempTimeX )
+    print( "P2-> Average waiting time : ", (waitMed / len( pList2 )), "Utilization Time : ",
+           int(((tempTimeY - idleMed) / tempTimeY) * 100), " Throughput", tpAl( 2 ), "Average Turnaround time :",
+           avTurnMed, "Idle time : ", t2, "Average Lenght : ", tempTimeY )
+    print( "P3-> Average waiting time : ", (waitLow / len( pList3 )), "Utilization Time : ",
+           int(((tempTimeZ - idleLow) / tempTimeX) * 100), " Throughput", tpAl( 3 ), "Average Turnaround time :",
+           avTurnLow, "Idle time : ", t3, "Average Lenght : ", tempTimeZ )
+
+
+    # for obj in pList:
+    # print(obj.ID, obj.start, obj.end, sep=' ')
+
+    """
+    Arrival Time: Time at which the process arrives in the ready queue.
+    Completion Time: Time at which process completes its execution.
+    Burst Time: Time required by a process for CPU execution.
+    Turn Around Time: Time Difference between completion time and arrival time.
+    Turn Around Time = Completion Time – Arrival Time
+
+    Waiting Time(W.T): Time Difference between turn around time and burst time.
+    Waiting Time = Turn Around Time – Burst Time
+    """
+def tpAl(priority):
+    tempSum = 0
+    counter = 0
+    if(priority == 1):
+        for obj in pList:
+            if (obj.end != -1):
+                tempSum += obj.end - obj.start
+                counter += 1
+    if (priority == 2):
+        for obj in pList2:
+            if (obj.end != -1):
+                tempSum += obj.end - obj.start
+                counter += 1
+    if (priority == 3):
+        for obj in pList3:
+            if (obj.end != -1):
+                tempSum += obj.end - obj.start
+                counter += 1
+
+    return int(tempSum/counter)
+
+
+
+
 
 def init(lamX,lamY,lamZ):
-    x1 = random.poisson(lam=lamX, size=10)
-    y1 = random.poisson(lam=10, size=10)
-    x2 = random.poisson(lam=lamY, size=10)
-    y2 = random.poisson(lam=10, size=10)
-    x3 = random.poisson(lam=lamZ, size=10)
-    y3 = random.poisson(lam=10, size=10)
 
+    x1,y1,x2,y2,x3,y3 = generatePoisson(lamX,lamY,lamZ)
 
+    global tempTimeX
+    global tempTimeY
+    global tempTimeZ
 
-    timeSum = req.reqTime(y1)
-    timeSum += req.reqTime(y2)
-    timeSum += req.reqTime(y3)
-    print("MasterClock : ", timeSum)
+    tempTimeX = req.reqTime(y1)
+    tempTimeY = req.reqTime(y2)
+    tempTimeZ = req.reqTime(y3)
+
+    timeSum = tempTimeX + tempTimeY + tempTimeZ
 
     global pList
     global pList2
@@ -187,13 +257,79 @@ def init(lamX,lamY,lamZ):
     return x1,y1,x2,y2,x3,y3,timeSum
 
 
-"""
-Arrival Time: Time at which the process arrives in the ready queue.
-Completion Time: Time at which process completes its execution.
-Burst Time: Time required by a process for CPU execution.
-Turn Around Time: Time Difference between completion time and arrival time.
-Turn Around Time = Completion Time – Arrival Time
+def generatePoisson(lamX,lamY,lamZ):
+    sizeHigh = 10
+    sizeMed = 10
+    sizeLow = 10
+    x1 = random.poisson( lam = lamX, size = sizeHigh )
+    for x in range(len(x1)):
+        while(x1[x] == 0):
+            x1[x] = random.poisson( lam = lamX, size = 1 )
+    y1 = random.poisson( lam = 10, size = sizeHigh )
+    for x in range(len(y1)):
+        while(y1[x] == 0):
+            y1[x] = random.poisson( lam = lamX, size = 1 )
+    x2 = random.poisson( lam = lamY, size = sizeMed )
+    for x in range(len(x2)):
+        while(x2[x] == 0):
+            x2[x] = random.poisson( lam = lamY, size = 1 )
+    y2 = random.poisson( lam = 10, size = sizeMed )
+    for x in range(len(y2)):
+        while(y2[x] == 0):
+            y2[x] = random.poisson( lam = lamY, size = 1 )
+    x3 = random.poisson( lam = lamZ, size = sizeLow )
+    for x in range(len(x3)):
+        while(x3[x] == 0):
+            x3[x] = random.poisson( lam = lamZ, size = 1 )
+    y3 = random.poisson( lam = 10, size = sizeLow )
+    for x in range(len(y3)):
+        while(y3[x] == 0):
+            y3[x] = random.poisson( lam = lamZ, size = 1 )
 
-Waiting Time(W.T): Time Difference between turn around time and burst time.
-Waiting Time = Turn Around Time – Burst Time
-"""
+    return x1,y1,x2,y2,x3,y3
+
+def endCheckMed():
+    for obj in pList2:
+        if(obj.end == -1):
+            return False
+    return True
+
+def runAndOptimize(runTime):
+    runTime = runTime % 30
+    highRatio = 0.5
+    medRatio = 0.3
+    lowRatio = 0.2
+    for x in range(runTime):
+        tempX1, tempY1, tempX2, tempY2, tempX3, tempY3, timeSum = init( 6, 7, 4 )
+        acc = 0.02
+        t1 = highPriority(tempX1, tempY1, (timeSum * highRatio))
+        t2 = medPriority(tempY2, (timeSum * medRatio))
+        t3 = lowPriority(tempY3, (timeSum * lowRatio))
+
+        if(t1 > t2 and t1 > t3):
+            if(t2 == 0 or t2 < t3):
+                highRatio -=acc
+                medRatio += acc
+            else:
+                highRatio -=acc
+                lowRatio += acc
+        elif(t2 > t3 and t2 > t1):
+            if(t3 == 0 or t3 > t1):
+                medRatio -= acc
+                lowRatio += acc
+            else:
+                medRatio -= acc
+                highRatio += acc
+        else:
+            print(t1,t2,t3)
+            if(t1 == 0 or t1 > t2):
+                lowRatio -= acc
+                highRatio += acc
+            else:
+                lowRatio -= acc
+                medRatio += acc
+        printLast(timeSum,t1,t2,t3)
+
+
+    return timeSum,t1,t2,t3
+
